60a52616f14ae93388ab5d6d77607e017d2af322
EnmanuelAbreu <enmanuelabreu393@gmail.com>
2026-05-29 16:21:58 -0400

    Poniendo la version del proyecto

--- Saludo/src/Main.java
+++ Saludo/src/Main.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static final String VERSION = "Version 1.1";
+
     public static void main(String[] args) {
         System.out.println("Hola, git!");
 
